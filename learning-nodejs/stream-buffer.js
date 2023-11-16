db41f0fc92e839ebc537270d83071d5bebd46bcd
anowarzz <anowarhosen444@gmail.com>
2023-11-16 21:19:48 +0600

    modified streaming codes

--- learning-nodejs/stream-buffer.js
+++ learning-nodejs/stream-buffer.js
@@ -1,25 +1,35 @@
 const http = require("http");
 
 // creating a server using raw node js
-
 const server = http.createServer();
 const fs = require("fs");
 
 // listener
 server.on("request", (req, res) => {
-  if ((req.url === "/read-file", req.method === "GET")) {
+  if (req.url === "/read-file" && req.method === "GET") {
+    // streaming file reading
+    const readableStream = fs.createReadStream(
+      process.cwd() + "/text/read.txt"
+    );
+
+    // streaming data
+    readableStream.on("data", (buffer) => {
+      res.statusCode = 200;
+      res.write(buffer);
+    
+    // ending streaming
+    readableStream.on("end", () => {
+      res.statusCode = 200;
+      res.end("The streaming is over!");
+    });
+
+    // on error while streaming
+    readableStream.on("error", (error) => {
+      console.log(error);
+      res.statusCode = 500;
+      res.end("Something went wrong!");
+    });
   }
-
-  // streaming file reading
-  const readableStream = fs.createReadStream(process.cwd() +"/text/read.txt");
-
-  readableStream.on("data", (buffer) => {
-    res.write(buffer);
-  });
-
-  readableStream.on("end", () => {
-    res.end("Hello from a place where snow falls like love");
-  });
 });
 
 server.listen(5000, () => {
