c28af6eae1aec2739cde635e05927829fbecb1e2
anowarzz <anowarhosen444@gmail.com>
2023-11-17 13:38:27 +0600

    middleware function revised

--- project-1-express/dist/app.js
+++ project-1-express/dist/app.js
@@ -9,12 +9,23 @@ const app = (0, express_1.default)();
 // parsers
 app.use(express_1.default.json());
 app.use(express_1.default.text());
+// middleware
+const logger = (req, res, next) => {
+    console.log(req.url, req.method, req.hostname);
+    next();
+};
 //get route root URL ("/")
-app.get("/", (req, res) => {
+app.get("/", logger, (req, res) => {
+    console.log(req.query);
     res.send("Hello developers");
 });
+//get route root URL ("/:userId")
+app.get("/:userId/:subId", (req, res) => {
+    console.log(req.params);
+    res.send("Dynamic world is here");
+});
 // post route root url('/')
-app.post("/", (req, res) => {
+app.post("/", logger, (req, res) => {
     console.log(req.body);
     res.json({
         message: "data received",
--- project-1-express/src/app.ts
+++ project-1-express/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response, Application } from "express";
+import express, { Request, Response, Application, NextFunction } from "express";
 
 // Create an instance of the Express application
 const app = express();
@@ -7,13 +7,27 @@ const app = express();
 app.use(express.json());
 app.use(express.text());
 
+// middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.url, req.method, req.hostname);
+
+  next();
+};
+
 //get route root URL ("/")
-app.get("/", (req: Request, res: Response) => {
+app.get("/", logger, (req: Request, res: Response) => {
+  console.log(req.query);
   res.send("Hello developers");
 });
 
+//get route root URL ("/:userId")
+app.get("/:userId/:subId", (req: Request, res: Response) => {
+  console.log(req.params);
+  res.send("Dynamic world is here");
+});
+
 // post route root url('/')
-app.post("/", (req: Request, res: Response) => {
+app.post("/", logger, (req: Request, res: Response) => {
   console.log(req.body);
   res.json({
     message: "data received",
