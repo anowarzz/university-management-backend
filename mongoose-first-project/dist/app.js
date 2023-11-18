abfada3f1db6029414e65b7a0692b77df2499fe6
anowarzz <anowarhosen444@gmail.com>
2023-11-18 10:32:10 +0600

    project setting  up for mongoose with express

---
+++ mongoose-first-project/dist/app.js
@@ -0,0 +1,10 @@
+"use strict";
+const express = require('express');
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+console.log(process.cwd());
+
+// F:\PHero-NextLevel\mongose-master-L2\mongoose-first-project/.env
