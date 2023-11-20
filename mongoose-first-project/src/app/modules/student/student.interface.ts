04fec641fc9fd7102b21507bf292605f9b3279e1
anowarzz <anowarhosen444@gmail.com>
2023-11-20 22:29:43 +0600

    creating a student type

---
+++ mongoose-first-project/src/app/modules/student/student.interface.ts
@@ -0,0 +1,30 @@
+import { Schema, model, connect } from 'mongoose';
+
+// Creating a type for guardian
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContactNo: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContactNo: string;
+};
+
+// Creating an Interface/Type for student
+export type Student = {
+  id: string;
+  name: {
+    firstName: string;
+    middleName: string;
+    lastName: string;
+  };
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+};
