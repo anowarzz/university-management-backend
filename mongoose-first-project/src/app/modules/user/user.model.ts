8046f24fa7ff03dc92a3c430d690222078fd3b5e
anowarzz <anowarhosen444@gmail.com>
2023-11-30 09:41:37 +0600

    created user module and defined user schema

---
+++ mongoose-first-project/src/app/modules/user/user.model.ts
@@ -0,0 +1,40 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+// Creating USER Schema
+const userSchema = new Schema<TUser>(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    needsPasswordChange: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ['student', 'faculty', 'admin'],
+    },
+    status: {
+      type: String,
+      enum: ['in-progress', 'blocked'],
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+
+
+// creating USER model
+export const User = model<TUser>('User', userSchema);
