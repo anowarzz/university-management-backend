4d675c8c568a9e7c5cb2bb774124f20ec64991b7
anowarzz <anowarhosen444@gmail.com>
2023-12-02 13:38:51 +0600

    organized the routes in a separated route file

--- mongoose-first-project/src/app.ts
+++ mongoose-first-project/src/app.ts
@@ -4,10 +4,9 @@
 import { NextFunction } from 'express';
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { StudentRoutes } from './app/modules/student/student.route';
-import { UserRoutes } from './app/modules/user/user.route';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -16,16 +15,18 @@ app.use(express.json());
 app.use(cors());
 
 // Application routes
-app.use('/api/v1/students', StudentRoutes);
-app.use('/api/v1/users', UserRoutes);
+app.use('/api/v1', router);
 
-app.get('/', (req: Request, res: Response) => {
+// Testing API HomeRoute
+const test = (req: Request, res: Response) => {
   res.status(200).json({
     success: true,
     message: 'Welcome to P-HERO University Server',
     note: 'Winter is coming',
   });
-});
+};
+
+app.get('/', test);
 
 // Error Handlers
 app.use(globalErrorHandler);
