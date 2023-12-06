de9a9dafb9b43f6561325f93c207fc0f6a35b82e
anowarzz <anowarhosen444@gmail.com>
2023-12-06 15:22:34 +0600

    fixed the issue to reset student id if the semester or year changes

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -16,14 +16,30 @@ const findStudentId = async () => {
     })
     .lean();
 
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // Generating a student id ==>
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  // first time id = 0000 ;
+  let currentId = (0).toString(); // 0000 by default ;
 
-  const currentId = (await findStudentId()) || (0).toString();
+  const lastStudentId = await findStudentId();
+
+  if (lastStudentId) {
+    const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+    const lastStudentYear = lastStudentId?.substring(0, 4);
+
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    if (
+      lastStudentId &&
+      lastStudentSemesterCode === currentSemesterCode &&
+      lastStudentYear === currentYear
+    ) {
+      currentId = lastStudentId.substring(6); // 0001
+    }
+  }
 
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
