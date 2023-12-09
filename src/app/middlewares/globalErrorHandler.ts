002aa3ada6b406c3a414db0f8d5fbe1a35b7b873
anowarzz <anowarhosen444@gmail.com>
2023-12-09 13:38:53 +0600

    creating customize error message for zod errors

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,22 +1,47 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express';
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-  err: any,
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Something went wrong';
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  // Setting default values
+  let statusCode = err.statusCode || 500;
+  let message = err.message || 'Something went wrong';
+
+  type TErrorSource = {
+    path: string | number;
+    message: string;
+  }[];
+
+  const errorSource: TErrorSource = [
+    {
+      path: '',
+      message: 'Something went wrong',
+    },
+  ];
+
+  if (err instanceof ZodError) {
+    statusCode = 400;
+    message = 'Ami zod error';
+  }
 
   return res.status(statusCode).json({
     success: false,
     message,
+    errorSource,
     error: err,
   });
 };
 
 export default globalErrorHandler;
+
+/*/ 
+success
+message
+errorSource: [
+  path: ' ',
+  message: ' '
+]
+stack
+ /*/
