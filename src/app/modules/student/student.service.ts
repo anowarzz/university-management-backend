9c11e13a0ab9d5a8dd3ec93b25e164d54098fb99
anowarzz <anowarhosen444@gmail.com>
2023-12-10 22:04:56 +0600

    added simple search term in find all student service

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -6,8 +6,20 @@ import { User } from '../user/user.model';
 import { TStudent } from './student.interface';
 
 //Get all students from DB
-const getAllStudentsFromDB = async () => {
-  const result = await Student.find()
+const getAllStudentsFromDB = async (query: Record<string, unknown>) => {
+  // {email: {$regex: query.searchTerm, $options: 'i'} }
+
+  let searchTerm = '';
+
+  if (query?.searchTerm) {
+    searchTerm = query?.searchTerm as string;
+  }
+
+  const result = await Student.find({
+    $or: ['email', 'name.firstName', 'presentAddress'].map((field) => ({
+      [field]: { $regex: searchTerm, $options: 'i' },
+    })),
+  })
     .populate('admissionSemester')
     .populate({
       path: 'academicDepartment',
