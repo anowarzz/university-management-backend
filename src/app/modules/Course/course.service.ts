2369708abb9c3ce1bb7e39d72a668811d1128565
anowarzz <anowarhosen444@gmail.com>
2023-12-15 10:39:56 +0600

    updated course service function to dynamically adddd pre requisite courses

--- src/app/modules/Course/course.service.ts
+++ src/app/modules/Course/course.service.ts
@@ -39,12 +39,38 @@ const updateCourseIntoDB = async (id: string, payload: Partial<TCourse>) => {
   const { preRequisiteCourses, ...courseRemainingData } = payload;
 
   // step -1 : basic course info update
-
   const updatedBasicCourseInfo = await Course.findByIdAndUpdate(
     id,
     courseRemainingData,
     { new: true, runValidators: true },
   );
+
+  // check if there is any preRequisite course to update
+  if (preRequisiteCourses && preRequisiteCourses.length > 0) {
+    // filter out the deleted fields
+    const deletedPreRequisites = preRequisiteCourses
+      .filter((el) => el.course && el.isDeleted)
+      .map((el) => el.course);
+
+    const deletedPreRequisitesCourses = await Course.findByIdAndUpdate(id, {
+      $pull: { preRequisiteCourses: { course: { $in: deletedPreRequisites } } },
+    });
+
+    // filter out the new course fields to add
+    const newPreRequisites = preRequisiteCourses?.filter(
+      (el) => el.course && !el.isDeleted,
+    );
+
+    const newPreRequisiteCourses = await Course.findByIdAndUpdate(id, {
+      $addToSet: { preRequisiteCourses: { $each: newPreRequisites } },
+    });
+
+    const result = await Course.findById(id).populate(
+      'preRequisiteCourses.course',
+    );
+
+    return result;
+  }
 
   return updatedBasicCourseInfo;
 };
