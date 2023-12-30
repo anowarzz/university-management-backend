45ed23521130c50c66e1214c201ca54183064af1
anowarzz <anowarhosen444@gmail.com>
2023-12-30 19:39:04 +0600

    created semester registration validation schema

---
+++ src/app/modules/semesterRegistration/semesterRegistration.validation.ts
@@ -0,0 +1,17 @@
+import { SemesterRegistrationStatus } from './semesterRegistration.constant';
+import { z } from 'zod';
+
+const createSemesterRegistrationValidationSchema = z.object({
+  body: z.object({
+    academicSemester: z.string(),
+    status: z.enum([...(SemesterRegistrationStatus as [string, ...string[]])]),
+    startDate: z.string().datetime(),
+    endDate: z.string().datetime(),
+    minCredit: z.number(),
+    maxCredit: z.number(),
+  }),
+});
+
+export const SemesterRegistrationValidations = {
+  createSemesterRegistrationValidationSchema,
+};
