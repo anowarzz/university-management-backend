7bfd1af0f16d31c087b1e3104eebec8a19abcec7
anowarzz <anowarhosen444@gmail.com>
2024-01-02 20:01:42 +0600

    created service and controller to get all semester rregistrations

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { SemesterRegistrationService } from './semesterRegistration.service';
 
+// creating a semester registration
 const createSemesterRegistration = catchAsync(async (req, res) => {
   const result =
     await SemesterRegistrationService.createSemesterRegistrationIntoDB(
@@ -18,6 +19,25 @@ const createSemesterRegistration = catchAsync(async (req, res) => {
   });
 });
 
+
+// Get all semesters
+const getAllSemesterRegistrations = catchAsync(async(req, res) => {
+  const result = await SemesterRegistrationService.getAllSemesterRegistrationsFromDB(req.query) ;
+
+    // sending response
+    sendResponse(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'Semester Registration Retrieved Successfully',
+      data: result,
+    });
+})
+
+
+
+
+
 export const SemesterRegistrationController = {
   createSemesterRegistration,
+  getAllSemesterRegistrations
 };
--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -3,7 +3,9 @@ import AppError from '../../errors/AppError';
 import { AcademicSemester } from '../academicSemester/academicSemester.model';
 import { TSemesterRegistration } from './semesterRegistration.interface';
 import { SemesterRegistration } from './semesterRegistration.model';
+import QueryBuilder from '../../builder/QueryBuilder';
 
+// ===> creating a semester registration into DB  ===>
 const createSemesterRegistrationIntoDB = async (
   payload: TSemesterRegistration,
 ) => {
@@ -36,6 +38,29 @@ const createSemesterRegistrationIntoDB = async (
   return result;
 };
 
+//  ===> Get All Semester Registration from DB ===>
+const getAllSemesterRegistrationsFromDB = async (
+  query: Record<string, unknown>,
+) => {
+  const semesterRegistrationQuery = new QueryBuilder(
+    SemesterRegistration.find().populate('academicSemester'),
+    query,
+  )
+    .filter()
+    .sort()
+    .paginate()
+    .fields();
+
+  const result = await semesterRegistrationQuery.modelQuery;
+
+  return result;
+};
+
+
+
+
+
 export const SemesterRegistrationService = {
   createSemesterRegistrationIntoDB,
+  getAllSemesterRegistrationsFromDB,
 };
